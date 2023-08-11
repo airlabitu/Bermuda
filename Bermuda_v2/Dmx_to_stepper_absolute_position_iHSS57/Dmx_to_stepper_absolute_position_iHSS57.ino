3c9156e7ed010c9e9e78907cedf6d29ef4836feb
Halfdan Hauch Jensen <Halfdanhauchjensen@gmail.com>
2023-08-11 19:24:32 +0200

    updated

--- Bermuda_v2/Dmx_to_stepper_absolute_position_iHSS57/Dmx_to_stepper_absolute_position_iHSS57.ino
+++ Bermuda_v2/Dmx_to_stepper_absolute_position_iHSS57/Dmx_to_stepper_absolute_position_iHSS57.ino
@@ -33,26 +33,93 @@ const int directionPin = 9;
 //AccelStepper stepper(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, directionPin);
 
-
+// SETTINGS
+// 230 cm travel in approc 9 sec
+// stepper driver - (5000 steps/revolution) 5:off 6:off 7:on 8:off
+// code - STEPPER_TOTAL_DISTANCE = 100000 
 
 const int STEPPER_TOTAL_DISTANCE = 100000;
 int newPosition = 0;
 
+int state = 0; 
+// 0 : free movement
+// 1 : stopping
+// 2 : retract from stop button
+// 3 : set to new zero
+
 void setup () {
+
+
   DMXSerial.init(DMXReceiver);
   // set a default value
   DMXSerial.write(DMX_ADDRESS, 0);
   
   stepper.setMaxSpeed(5000);  // max speed
-  stepper.setAcceleration(1000); // acceleration
-  stepper.moveTo(1000); // absolute position to move to, just for testing at start up.  
+  stepper.setAcceleration(10000); // acceleration
+  //stepper.moveTo(1000); // absolute position to move to, just for testing at start up.  
+
+  pinMode(4, INPUT_PULLUP);
     
 }
 
 
 void loop() {
+  int sensorVal = digitalRead(4);
   stepper.run();
-  
+  switch(state){
+    case 0:
+      if (sensorVal == 1){
+        // Calculate how long no data packet was received
+        unsigned long lastPacket = DMXSerial.noDataSince();
+        // check if DMX is still being received
+        if (lastPacket < 5000) { 
+          newPosition = map(DMXSerial.read(DMX_ADDRESS), 0, 255, 0, STEPPER_TOTAL_DISTANCE); // calculate new position based on DMX data
+        stepper.moveTo(newPosition); // go to new position
+        }
+      }
+
+      else { // button is pressed
+        stepper.stop();
+        stepper.setCurrentPosition(100);
+        state = 1;
+      }
+      break;
+    
+    case 1:
+      stepper.run();
+      stepper.moveTo(0);
+      //if (sensorVal == 1) state = 2;
+      
+      /*if (!stepper.isRunning()){ // wait for stepper to stop (it de-accelerates)
+        state = 2;
+      }*/
+      //stepper.run();
+      break;
+    
+    case 2:
+      //stepper.run();
+      //stepper.setCurrentPosition(0);
+      //stepper.moveTo(200);
+      //stepper.move(500);
+      //stepper.run();
+      //state = 3;
+      break;
+    
+    /*
+    case 3:
+      if (!stepper.isRunning()){
+        stepper.setCurrentPosition(0);
+        state = 4;
+      }
+      break;
+    
+    case 4:
+      if (sensorVal = 0) state = 0;
+      break;
+    */
+  }
+
+  /*
   // Calculate how long no data packet was received
   unsigned long lastPacket = DMXSerial.noDataSince();
   
@@ -61,4 +128,5 @@ void loop() {
     newPosition = map(DMXSerial.read(DMX_ADDRESS), 0, 255, 0, STEPPER_TOTAL_DISTANCE); // calculate new position based on DMX data
     stepper.moveTo(newPosition); // go to new position
   }
+  */
 }
